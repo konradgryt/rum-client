8d21beeeb7edd8d94f1409a294bff18f849a02ba
Konrad <k.gryt@gazeta.pl>
2018-05-22 06:00:26 +0200

    cardlist component

--- src/components/cardlist.jsx
+++ src/components/cardlist.jsx
@@ -10,13 +10,29 @@ class CardList extends React.Component {
   }
 
   componentDidMount() {
-
+    Utility.get('/rum').then(json => {
+      console.log(json);
+        this.setState({
+          list: json
+        });
+      });
   }
 
   render() {
     return (
       <section>
-        <Rum />
+          <list items={this.state.list} onRenderCell={(items) => (
+            <section>
+                <Rum name={items.name}
+                 image={items.image}
+                 percentage={items.percentage}
+                 yearOfBrew={items.yearOfBrew}
+                 producer={items.producer}
+                 typeOfRum={items.typeOfRum}
+                 kettleType={items.kettleType}
+                 reviews={items.reviews} />
+            </section>
+        )}/>
       </section>
     );
   }
