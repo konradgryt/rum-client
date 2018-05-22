7d91b2a7fb4bf1be530ea77b00fc1aef7fff6dff
Matiss Cikota <matisscikota@gmail.com>
2018-05-22 12:22:25 +0200

    styling

--- src/components/pane/panebody.jsx
+++ src/components/pane/panebody.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import Rum from './rum';
+
 //import Utility from '../logic/utility';
 
 class PaneBody extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-  render() {
-    return (
-      <section className='panebody'>
-          <i className="material-icons">close</i>
-          <Rum />
-      </section>
-    );
-  }
+    render() {
+        return (
+            <section className='panebody'>
+                <i className="material-icons">close</i>
+                <Rum/>
+            </section>
+        );
+    }
 }
 
 export default PaneBody;
