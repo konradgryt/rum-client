5f1bc20146046c5fce00031a99ef2f15fce41518
Matiss Cikota <matisscikota@gmail.com>
2018-05-22 07:38:19 +0200

    Fixes + styling

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,70 +3,61 @@ import Utility from '../logic/utility';
 
 class Login extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {};
+    constructor(props) {
+        super(props);
+        this.state = {};
 
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  handleUsernameChange(event) {
-    this.setState({username: event.target.value});
-  }
+    handleUsernameChange(event) {
+        this.setState({username: event.target.value});
+    }
 
-  handlePasswordChange(event) {
-    this.setState({password: event.target.value});
-  }
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
 
-  handleSubmit(event) {
-    var data = JSON.stringify ({
-        username: this.state.username,
-        password: this.state.password
-    });
-    Utility.post('/user/login', data ).then(json => {
-      console.log(json);
-        if (json) {
-          this.props.view.renderOverview(this.state.username);
-        } else {
-          this.setState({
-              message : this.ErrorMessage
-          });
-        }
-    });
-    event.preventDefault();
-  }
+    handleSubmit(event) {
+        var data = JSON.stringify({
+            username: this.state.username,
+            password: this.state.password
+        });
+        Utility.post('/user/login', data).then(json => {
+            console.log(json);
+            if (json) {
+                this.props.view.renderOverview(this.state.username);
+            } else {
+                this.setState({
+                    message: this.ErrorMessage
+                });
+            }
+        });
+        event.preventDefault();
+    }
 
-  ErrorMessage = (
-    <div> 
-      error
-    </div>
-  );
-
-  render() {
-    return (
-      <section className='login'>
-        <form onSubmit={this.handleSubmit}>
-            <div>Login</div>
-          <div>
-            <label>Username:
-              <input type='text' id='username' onChange={this.handleUsernameChange} />
-            </label>
-          </div>
-          <div> 
-            <label>Password:
-              <input type='password' onChange={this.handlePasswordChange}  />
-            </label>
-            <input type='submit' id='password' value='ok' />
-          </div>
-          <div>
-            {this.state.message}
-          </div>
-        </form>
-      </section>
+    ErrorMessage = (
+        <div>
+            error
+        </div>
     );
-  }
+
+    render() {
+        return (
+            <section className='login'>
+                <form onSubmit={this.handleSubmit}>
+                    <input type='submit' id='password' value='Login'/>
+                    <input type='text' id='username' onChange={this.handleUsernameChange} placeholder='Username'/>
+                    <input type='password' onChange={this.handlePasswordChange} placeholder='Password'/>
+                    <div>
+                        {this.state.message}
+                    </div>
+                </form>
+            </section>
+        );
+    }
 }
 
 export default Login;
--- src/components/rum.jsx
+++ src/components/rum.jsx
@@ -3,79 +3,87 @@ import Utility from '../logic/utility';
 
 class Rum extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {};
+    constructor(props) {
+        super(props);
+        this.state = {};
 
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleImageChange = this.handleImageChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleImageChange = this.handleImageChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  handleNameChange(event) {
-    this.setState({name: event.target.value});
-  }
+    handleNameChange(event) {
+        this.setState({name: event.target.value});
+    }
 
-  handleImageChange(event) {
-    var input = event.target.files[0];
+    handleImageChange(event) {
+        var input = event.target.files[0];
 
-    this.setState({image: input});
-  }
+        this.setState({image: input});
+    }
 
-  handleSubmit(event) {
-    var data = new FormData();
-    data.append('name' ,this.state.name);
-    data.append('image', this.state.image);
-    console.log(data);
-    Utility.post('/rum', data ).then(json => {
-      console.log(json);
-        if (json) {
-            this.setState({
-                message : this.SuccessMessage
+    handleSubmit(event) {
+        let data = new FormData();
+        const fileField = document.querySelector('input[type="file"]');
+
+        data.append('name', this.state.name);
+        data.append('image', fileField.files[0]);
+
+        fetch('http://192.168.0.100:8080/rum', {
+            method: 'POST',
+            body: data
+        })
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                if (json) {
+                    this.setState({
+                        message: this.SuccessMessage
+                    });
+                } else {
+                    this.setState({
+                        message: this.ErrorMessage
+                    });
+                }
             });
-        } else {
-            this.setState({
-                message : this.ErrorMessage
-            });
-        }
-    });
-    event.preventDefault();
-  }
+        event.preventDefault();
+    }
 
-  SuccessMessage = (
-    <div> 
-        RUM ADDED MY MAN
-    </div>
-  );
-
-  ErrorMessage = (
-    <div> 
-        sadface
-    </div>
-  );
-
-  render() {
-    return (
-      <section className='rum'>
-        <form onSubmit={this.handleSubmit}>
-            <h1> RUm</h1>
-          <div> 
-            <label>Name
-              <input id='name' type='text' onChange={this.handleNameChange}  />
-            </label>
-            <label>jpg
-                <input hidden id='image-location' type='file' accept='image/*' onChange={this.handleImageChange}  />
-                <button onClick={() => document.getElementById('image-location').click()}>...</button>
-            </label>
-           <input type='submit' value='ok' />
-          </div>
-          <div className='mainpage__input'>
-            {this.state.message}
-          </div>
-        </form>
-      </section>
+    SuccessMessage = (
+        <div>
+            RUM ADDED MY MAN
+        </div>
     );
-  }
+
+    ErrorMessage = (
+        <div>
+            sadface
+        </div>
+    );
+
+    render() {
+        return (
+            <section className='rum'>
+                <form onSubmit={this.handleSubmit}>
+                    <h1>Rum</h1>
+                    <div>
+                        <label>Name
+                            <input id='name' type='text' onChange={this.handleNameChange}/>
+                        </label>
+                        <label>jpg
+                            <input hidden id='image-location' type='file' accept='image/*'
+                                   onChange={this.handleImageChange}/>
+                            <button onClick={() => document.getElementById('image-location').click()}>...</button>
+                        </label>
+                        <input type='submit' value='ok'/>
+                    </div>
+                    <div className='mainpage__input'>
+                        {this.state.message}
+                    </div>
+                </form>
+            </section>
+        );
+    }
 }
 
 export default Rum;
