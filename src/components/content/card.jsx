e4a278ec2c5fa499507e37acc471d611a38cc663
Konrad <k.gryt@gazeta.pl>
2018-05-22 11:09:49 +0200

    some stuffsss

--- src/components/content/card.jsx
+++ src/components/content/card.jsx
@@ -13,23 +13,24 @@ class Card extends React.Component {
   render() {
     return (
       <section className='card'>
-        <div> Rum card: </div>
-        <div> Name {this.props.name}</div>
-        <div> Image {this.props.image}</div>
-        <div> % {this.props.percentage}</div>
-        <div> year of brew {this.props.yearOfBrew}</div>
-        <div> producer {this.props.producer}</div>
-        <div> type of rum {this.props.typeOfRum}</div>
-        <div> kettle type {this.props.kettleType}</div>
-        <div> reviews: 
-        {this.props.reviews.map(items =>
-              <Review 
-                 image = {items.image}
-                 rumId = {items.rumId}
-                 userId=  {items.userId}
-                 rating = {items.userId}
-                 additionalInfo = {items.additionalInfo}
-            />)}
+        <div>{this.props.image}</div>
+        <div className='card__description'> 
+          <div>Name{this.props.name}</div>
+          <div>% {this.props.percentage}</div>
+          <div>year of brew {this.props.yearOfBrew}</div>
+          <div>producer {this.props.producer}</div>
+          <div>type of rum {this.props.typeOfRum}</div>
+          <div>kettle type {this.props.kettleType}</div>
+          <div>reviews: 
+          {this.props.reviews.map(items =>
+                <Review 
+                  image = {items.image}
+                  rumId = {items.rumId}
+                  userId=  {items.userId}
+                  rating = {items.userId}
+                  additionalInfo = {items.additionalInfo}
+              />)}
+          </div>
         </div>
       </section>
     );
